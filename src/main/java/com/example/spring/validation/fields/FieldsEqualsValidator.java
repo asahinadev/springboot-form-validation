5bf522d6c34a661def0459e492f7908949f3355a
asahinadev <dev.account.01@mail.mirageworld.jp>
2019-05-22 11:55:27 +0900

    ２項目一致チェック

---
+++ src/main/java/com/example/spring/validation/fields/FieldsEqualsValidator.java
@@ -0,0 +1,40 @@
+package com.example.spring.validation.fields;
+
+import java.util.Objects;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.example.spring.validation.fields.property.FieldsProperty;
+
+public class FieldsEqualsValidator implements ConstraintValidator<FieldsEquals, FieldsProperty<?>> {
+	FieldsEquals annotation;
+
+	@Override
+	public void initialize(FieldsEquals annotation) {
+		this.annotation = annotation;
+	}
+
+	@Override
+	public boolean isValid(FieldsProperty<?> form, ConstraintValidatorContext context) {
+		if (form == null) {
+			return true;
+		}
+
+		Object field1 = form.getMain();
+		Object field2 = form.getSub();
+
+		if (Objects.equals(field1, field2)) {
+			return true;
+		}
+
+		context.disableDefaultConstraintViolation();
+		context.buildConstraintViolationWithTemplate(annotation.message())
+				.addPropertyNode(annotation.fieldSub())
+				.addConstraintViolation();
+
+		return false;
+
+	}
+
+}
---
+++ src/main/java/com/example/spring/validation/fields/property/FieldsProperty.java
@@ -0,0 +1,9 @@
+package com.example.spring.validation.fields.property;
+
+public interface FieldsProperty<E> {
+
+	public E getMain();
+
+	public E getSub();
+
+}
