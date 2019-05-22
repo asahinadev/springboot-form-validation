71d8d6e0fe61c081fc0a6ce304812d61489231ca
asahinadev <dev.account.01@mail.mirageworld.jp>
2019-05-22 11:45:41 +0900

    半角数ｎ字チェック

---
+++ src/main/java/com/example/spring/validation/characters/Numeric.java
@@ -0,0 +1,38 @@
+package com.example.spring.validation.characters;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = NumericValidator.class)
+@Repeatable(Numeric.List.class)
+@Documented
+public @interface Numeric {
+
+	String message() default "{com.example.spring.validation.Numeric.message}";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+
+	@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+	@Retention(RetentionPolicy.RUNTIME)
+	@Documented
+	public @interface List {
+		Numeric[] value();
+	}
+}
