83a0b980861097369db578d3d395588d1c6e9a1e
asahinadev <dev.account.01@mail.mirageworld.jp>
2019-05-27 10:46:04 +0900

    前後共通処理

--- src/test/java/com/example/spring/SpringParameterized.java
+++ src/test/java/com/example/spring/SpringParameterized.java
@@ -1,5 +1,7 @@
 package com.example.spring;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
@@ -9,18 +11,56 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.rules.SpringClassRule;
 import org.springframework.test.context.junit4.rules.SpringMethodRule;
+import org.springframework.validation.BindingResult;
 
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @ActiveProfiles("test")
 @ComponentScan(basePackages = {
 		"com.example.spring"
 })
 @RunWith(Parameterized.class)
 @SpringBootTest
-public class SpringParameterized {
+public abstract class SpringParameterized {
 
 	@ClassRule
 	public static final SpringClassRule RULE = new SpringClassRule();
 
 	@Rule
 	public final SpringMethodRule rule = new SpringMethodRule();
+
+	protected BindingResult result;
+
+	@ToString
+	@RequiredArgsConstructor
+	public static class Parameter {
+		final public String value;
+		final public int errorCount;
+	}
+
+	final protected Parameter parameter;
+
+	public SpringParameterized(Parameter parameter) {
+		this.parameter = parameter;
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		log.info("{}", parameter);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		if (result != null) {
+			log.info("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ");
+			result.getFieldErrors().stream().forEach(item -> {
+				log.info("{} : {}", item.getField(), item.getDefaultMessage());
+			});
+		}
+		log.info("------------------------------------------------------------");
+	}
+
 }
