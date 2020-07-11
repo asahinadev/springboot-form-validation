6f16511678b1a2b7e49b35cc9d059b1157d0a501
朝日奈ありす <59528169+asahinadev@users.noreply.github.com>
2020-07-11 14:04:01 +0900

    カバレッジ１００％

--- src/test/java/com/example/spring/validation/zipcode/ZipCodeValidatorTest.java
+++ src/test/java/com/example/spring/validation/zipcode/ZipCodeValidatorTest.java
@@ -2,16 +2,62 @@ package com.example.spring.validation.zipcode;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.validation.*;
+
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.*;
 import org.junit.jupiter.params.provider.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.test.context.*;
 import org.springframework.validation.*;
+import org.springframework.validation.Validator;
 
 import lombok.*;
 
 @SpringBootTest
 class ZipCodeValidatorTest {
+
+	@AllArgsConstructor
+	class Form2 {
+		@ZipCode(fields = "code1")
+		Zip zip;
+	}
+
+	@AllArgsConstructor
+	class Form3 {
+		@ZipCode(fields = { "code1", "code2" })
+		Zip zip;
+	}
+
+	@AllArgsConstructor
+	class Form4 {
+		@ZipCode(fields = { "code1", "code2", "code3" })
+		Zip zip;
+	}
+
+	class Zip {
+		@ZipCode(fields = "code")
+		String code;
+
+		@Getter
+		String code1;
+
+		@Getter
+		String code2;
+
+		public Zip(String code1, String code2) {
+			this.code1 = code1;
+			this.code2 = code2;
+		}
+
+		public Zip(String code1) {
+			this.code1 = code1;
+		}
+
+		public Zip(String code, boolean a) {
+			this.code = code;
+		}
+	}
 
 	@Autowired
 	Validator validator;
@@ -22,18 +68,7 @@ class ZipCodeValidatorTest {
 			"123-4567"
 	})
 	void ok1(String value) {
-		@Data
-		@AllArgsConstructor
-		class Zip {
-			@ZipCode(fields = "code")
-			String code;
-		}
-		Zip form = new Zip(value);
-
-		BindingResult r = new BindException(form, "form");
-		validator.validate(form, r);
-
-		assertEquals(r.getErrorCount(), 0);
+		check(new Zip(value, false), 0);
 	}
 
 	@ParameterizedTest
@@ -44,18 +79,7 @@ class ZipCodeValidatorTest {
 			"123-567"
 	})
 	void error1(String value) {
-		@Data
-		@AllArgsConstructor
-		class Zip {
-			@ZipCode(fields = "code")
-			String code;
-		}
-		Zip form = new Zip(value);
-
-		BindingResult r = new BindException(form, "form");
-		validator.validate(form, r);
-
-		assertEquals(r.getErrorCount(), 1);
+		check(new Zip(value, false), 1);
 	}
 
 	@ParameterizedTest
@@ -64,25 +88,7 @@ class ZipCodeValidatorTest {
 			"123-4567"
 	})
 	void ok2(String value) {
-		@Data
-		@AllArgsConstructor
-		class Zip {
-			String code;
-		}
-
-		@Data
-		@AllArgsConstructor
-		class Form {
-			@ZipCode(fields = "code")
-			Zip zip;
-		}
-
-		Form form = new Form(new Zip(value));
-
-		BindingResult r = new BindException(form, "form");
-		validator.validate(form, r);
-
-		assertEquals(r.getErrorCount(), 0);
+		check(new Form2(new Zip(value)), 0);
 	}
 
 	@ParameterizedTest
@@ -93,25 +99,7 @@ class ZipCodeValidatorTest {
 			"123-567"
 	})
 	void error2(String value) {
-		@Data
-		@AllArgsConstructor
-		class Zip {
-			String code;
-		}
-
-		@Data
-		@AllArgsConstructor
-		class Form {
-			@ZipCode(fields = "code")
-			Zip zip;
-		}
-
-		Form form = new Form(new Zip(value));
-
-		BindingResult r = new BindException(form, "form");
-		validator.validate(form, r);
-
-		assertEquals(r.getErrorCount(), 1);
+		check(new Form2(new Zip(value)), 1);
 	}
 
 	@ParameterizedTest
@@ -119,54 +107,45 @@ class ZipCodeValidatorTest {
 			"123,4567"
 	})
 	void ok3(String code1, String code2) {
-		@Data
-		@AllArgsConstructor
-		class Zip {
-			String code1;
-			String code2;
-		}
-
-		@Data
-		@AllArgsConstructor
-		class Form {
-			@ZipCode(fields = { "code1", "code2" })
-			Zip zip;
-		}
-
-		Form form = new Form(new Zip(code1, code2));
-
-		BindingResult r = new BindException(form, "form");
-		validator.validate(form, r);
-
-		assertEquals(r.getErrorCount(), 0);
+		check(new Form3(new Zip(code1, code2)), 0);
 	}
 
 	@ParameterizedTest
-	@CsvSource({
-			"1234,1234",
-			"123,123",
-	})
+	@CsvSource({ "1234,1234", "123,123" })
 	void error3(String code1, String code2) {
-		@Data
-		@AllArgsConstructor
-		class Zip {
-			String code1;
-			String code2;
-		}
-
-		@Data
-		@AllArgsConstructor
-		class Form {
-			@ZipCode(fields = { "code1", "code2" })
-			Zip zip;
-		}
-
-		Form form = new Form(new Zip(code1, code2));
-
-		BindingResult r = new BindException(form, "form");
-		validator.validate(form, r);
-
-		assertEquals(r.getErrorCount(), 1);
+		check(new Form3(new Zip(code1, code2)), 1);
 	}
 
+	@Test
+	void ok1() {
+		ok1(null);
+	}
+
+	@Test
+	void ok2() {
+		ok2(null);
+	}
+
+	@Test
+	void ok3() {
+		ok3(null, null);
+	}
+
+	@Test
+	void error3() {
+		check(new Form3(new Zip("123", null)), 1);
+		check(new Form3(new Zip(null, "1234")), 1);
+	}
+
+	@Test
+	void error4() {
+		assertThrows(ValidationException.class,
+				() -> check(new Form4(new Zip(null, null)), 1));
+	}
+
+	private void check(Object form, int errorCount) {
+		BindingResult r = new BindException(form, "form");
+		validator.validate(form, r);
+		assertEquals(r.getErrorCount(), errorCount);
+	}
 }
